a1bc43ef3ec947f565801f3f11b9de21e1ae0383
Wegleton Marinheiro <wegleton_19@outlook.com>
2022-11-08 13:54:28 -0300

    first commit

---
+++ JavaScript/challenge/script.js
@@ -0,0 +1,8 @@
+let userName = prompt("Digite seu Nome:")
+alert("Olá, " + userName + ", Vamos Fazer uma Soma!")
+
+let firstNumber = Number(prompt("Digite o primeiro Numero: "))
+let secondNumber = Number(prompt("Digite o segundo Numero: "))
+let sum = firstNumber + secondNumber
+
+alert(`A soma do ${firstNumber} e o ${secondNumber} é ${sum}`)
---
+++ JavaScript/study/script.js
@@ -0,0 +1,29 @@
+// segundo o ECMAScript
+
+// >>> ## primitive volues (String, Number, Boolean)
+
+// String
+
+let names = "Wegleton"
+let age = 29
+// funções construtoras para converter tipo
+//age = number(age)
+
+console.log(names)
+console.log(typeof names)
+
+console.log(age)
+console.log(typeof age)
+
+let num1 = 1
+let num2 = 5
+let soma = num1 + num2
+
+console.log(soma)
+//console.log(typeof soma)
+
+// >>> Variables Scope (Hoisting) <<<
+
+let num = 2
+console.log(num)
+
